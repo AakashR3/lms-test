1a9e683290b2260b4864c019c8d34bcd4733e2f4
Ramki A <kvraamkeydev@gmail.com>
2022-09-11 00:48:13 +0530

    auth changes

--- src/features/authentication/layout/index.tsx
+++ src/features/authentication/layout/index.tsx
@@ -1,11 +1,7 @@
 import { Outlet } from '@tanstack/react-location';
-import { useEffect } from 'react';
 import { Slider } from '../components/Slider';
 
 function AuthLayout() {
-	useEffect(() => {
-		localStorage.clear();
-	}, []);
 	return (
 		<section className="h-full w-full flex">
 			<Slider />
