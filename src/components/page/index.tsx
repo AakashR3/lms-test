ae36ca5c1e25dd70b1a3ef3505722e15d74a3d5b
davakumar@codincity.com <davakumar@codincity.com>
2022-10-21 11:17:24 +0530

    Padding top issue

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -6,7 +6,7 @@ export interface IPage {
 function Page(props: PropsWithChildren<IPage>) {
 	const { title, children } = props;
 	return (
-		<section className="main w-full px-[var(--margin-x)]">
+		<section className="main w-full px-[var(--margin-x)] py-5">
 			{title && (
 				<div className="flex items-center space-x-4 py-5 lg:py-6">
 					<h2 className="text-xl font-medium text-slate-800 dark:text-navy-50 lg:text-2xl">{title}</h2>
